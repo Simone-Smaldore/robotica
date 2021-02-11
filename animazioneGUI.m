cec864eb63c9f988736700d85c9592b46d81b10a
Giuseppe Montanaro <giuseppe.montanaro@svelto.tech>
2021-02-11 11:22:35 +0100

    ≈ commit super importantissimo

--- animazioneGUI.m
+++ animazioneGUI.m
@@ -1,4 +1,4 @@
-function [] = animazioneGUI(parametri, tempi, tipologie, uiaxes, salvaVideo)
+function [] = animazioneGUI(parametri, tempi, tipologie, riferimenti, uiaxes, salvaVideo)
     disp('Animazione in esecuzione...')
     if (salvaVideo)
         count = 1;
@@ -6,42 +6,62 @@ function [] = animazioneGUI(parametri, tempi, tipologie, uiaxes, salvaVideo)
         frames = struct('cdata', cell(1,num_frames), 'colormap', cell(1,num_frames));
     end
     for i = 1 : size(tempi, 2) - 1
-       t = linspace(tempi(i), tempi(i + 1));
-       for j = 1 : size(t, 2)
-           q = zeros(size(parametri, 1));
-           for k = 1 : size(parametri, 1)
-               poly = parametri(k, i, :);
-               polyVec = reshape(poly, [1,4]);
-               q(k) = polyval(polyVec, t(j));
-               if (k == 1)
-                   x(k) = 0;
-                   y(k) = cosd(q(k));
-                   z(k) = sind(q(k));
-               else
-                   x(k) = x(k-1) + cosd(q(k));
-                   y(k) = y(k-1) + sind(q(k));
-                   z(k) = z(k-1);
-               end
-           end
+        t = linspace(tempi(i), tempi(i + 1));
+        for j = 1 : size(t, 2)
+            q = zeros(size(parametri, 1));
+            x = [0];
+            y = [0];
+            z = [0];
+            for k = 1 : size(parametri, 1)
+                poly = parametri(k, i, :);
+                polyVec = reshape(poly, [1,4]);
+                q(k) = polyval(polyVec, t(j));
+                
+                tipo = tipologie{k};
+                ref = riferimenti{k};
+                if strcmp(tipo, 'Rotoidale')
+                    if strcmp(ref, 'x')
+                        x(k+1) = x(k);
+                        y(k+1) = y(k) + cosd(q(k));
+                        z(k+1) = z(k) + sind(q(k));
+                    elseif strcmp(ref, 'y')
+                        x(k+1) = x(k) + cosd(q(k));
+                        y(k+1) = y(k);
+                        z(k+1) = z(k) + sind(q(k));
+                    else
+                        x(k+1) = x(k) + cosd(q(k));
+                        y(k+1) = y(k) + sind(q(k));
+                        z(k+1) = z(k);
+                    end
+                else
+                   if strcmp(ref, 'x')
+                        x(k+1) = q(k) + x(k);
+                    elseif strcmp(ref, 'y')
+                        y(k+1) = q(k) + y(k);
+                   else
+                        z(k+1) = q(k) + z(k);
+                   end
+                end
+            end
            
-           for k = 1 : size(parametri, 1) - 1
-               plot3(uiaxes, [0, x(k), x(k+1)], [0, y(k), y(k+1)], [0, z(k), z(k+1)], 'linewidth', 2);
-               hold(uiaxes, 'on');
-               shadeColor = [.85,.85,.85];
-               plot3(uiaxes, [0, x(k), x(k+1)], [0, y(k), y(k+1)], [0, 0, 0],'-','Color',shadeColor,'LineWidth',2); 
-               plot3(uiaxes, [0, x(k), x(k+1)], [2, 2, 2], [0, z(k), z(k+1)],'-','Color',shadeColor,'LineWidth',2); 
-               plot3(uiaxes, [2, 2, 2], [0, y(k), y(k+1)], [0, z(k), z(k+1)],'-','Color',shadeColor,'LineWidth',2);
-               hold(uiaxes, 'off');
-           end
-           axis([-2 2 -2 2 0 4]);
-           grid on
-           %pause(0.01)
-           pause(tempi(i+1) / size(t, 2));
-           if (salvaVideo)
-               frames(count) = getframe(gcf);
-               count = count + 1;
-           end
-       end
+            for k = 1 : size(parametri, 1) - 1
+                plot3(uiaxes, [x(k), x(k+1)], [y(k), y(k+1)], [z(k), z(k+1)], 'linewidth', 2);
+                hold(uiaxes, 'on');
+                shadeColor = [.85,.85,.85];
+                plot3(uiaxes, [x(k), x(k+1)], [y(k), y(k+1)], [0, 0],'-','Color',shadeColor,'LineWidth',2); 
+                plot3(uiaxes, [x(k), x(k+1)], [2, 2], [z(k), z(k+1)],'-','Color',shadeColor,'LineWidth',2); 
+                plot3(uiaxes, [2, 2], [y(k), y(k+1)], [z(k), z(k+1)],'-','Color',shadeColor,'LineWidth',2);
+                hold(uiaxes, 'off');
+            end
+            
+            grid on
+%             pause(0.01)
+            pause(tempi(i+1) / size(t, 2));
+            if (salvaVideo)
+                frames(count) = getframe(gcf);
+                count = count + 1;
+            end
+        end
     end
     
     if (salvaVideo)
